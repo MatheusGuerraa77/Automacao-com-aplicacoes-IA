9c6a34572363e2f678d75ebc7b9ec3f80df187da
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-11 19:43:20 -0300

    Update 1-analise_texto.py

--- 1-analise_texto.py
+++ 1-analise_texto.py
@@ -1,6 +1,6 @@
 import os
 
-# 1 - Importação do texto
+# 1 - importação do texto
 with open(os.path.join('data', 'texto.txt'), 'r', encoding='utf-8') as file:
     texto = file.read()
     print(texto)
