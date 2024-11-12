1c0f87bb4fbd016f38d52eafab47055146c9c250
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-12 11:42:29 -0300

    Update 1-analise_texto.py

--- 1-analise_texto.py
+++ 1-analise_texto.py
@@ -1,7 +1,28 @@
 import os
+import nltk
+from nltk import sent_tokenize
+from nltk import word_tokenize
+from nltk.probability import FreqDist
 
-# 1 - importação do texto
+# nltk.download('punkt_tab')
+
+
+# 1 - Importação do texto
 with open(os.path.join('data', 'texto.txt'), 'r', encoding='utf-8') as file:
     texto = file.read()
     print(texto)
     
+# 2 - Tokenizando o Texto
+sent_tokens = sent_tokenize(texto)
+print(sent_tokens)
+print(len(sent_tokens))
+
+word_tokens = word_tokenize(texto) 
+print(word_tokens)
+print(len(word_tokens))  
+
+# 3 - Frequência de Distribuição
+fdist = FreqDist(word_tokens)
+# print(fdist)
+print(fdist.most_common(10))
+fdist.plot(10)
