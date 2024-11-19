65aac4bdd0f42e3f379613c1caf36c968605fc73
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-18 23:18:12 -0300

    Update 2-analise_texto_web.py

--- 2-analise_texto_web.py
+++ 2-analise_texto_web.py
@@ -1,4 +1,10 @@
 from goose3 import Goose
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+
+# nltk.download('stopwords')
 
 # 1 - Importando Artigo da Internet
 g = Goose()
@@ -9,3 +15,19 @@ print(artigo._title)
 print(artigo.meta_description)
 print(artigo.links)
 print(artigo.cleaned_text)
+
+# 2 - Aplicando Ánalise Textual I
+word_tokens = word_tokenize(artigo.cleaned_text)
+print(word_tokens)
+print(len(word_tokens))
+
+portuguese_stops = (set(stopwords.words('portuguese')))
+
+# for palavra in word_tokens:
+#     if palavra.lower() not in portuguese_stops:
+#         print(palavra)
+#         print(len(palavra))
+
+palavras = [palavra for palavra in word_tokens if palavra.lower() not in portuguese_stops]
+print(palavras)
+print(len(palavras))
