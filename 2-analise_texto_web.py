329a4de194da893eac27c0a551cd96c99657bb36
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-18 23:28:32 -0300

    Update 2-analise_texto_web.py

--- 2-analise_texto_web.py
+++ 2-analise_texto_web.py
@@ -2,6 +2,11 @@ from goose3 import Goose
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+import numpy as np
 
 
 # nltk.download('stopwords')
@@ -31,3 +36,29 @@ portuguese_stops = (set(stopwords.words('portuguese')))
 palavras = [palavra for palavra in word_tokens if palavra.lower() not in portuguese_stops]
 print(palavras)
 print(len(palavras))
+
+# 3 - Aplicando Análise Textual II
+fdist = FreqDist(palavras)
+# print(fdist.most_common(10))
+
+novas_palavras = [palavra for palavra in palavras if palavra.isalnum()]
+fdist = FreqDist(novas_palavras)
+print(fdist.most_common(10))
+
+def plot_cloud(worldcloud):
+    plt.figure(figsize=(40, 30))
+    plt.imshow(worldcloud)
+    plt.axis('off')
+    plt.show()
+    
+wordcloud = WordCloud(
+    width = 3000,
+    height = 2000,
+    random_state = 1,
+    background_color = 'navy',
+    colormap = 'rainbow',
+    collocations = False,
+    stopwords = STOPWORDS 
+).generate(' '.join(novas_palavras))
+
+plot_cloud(wordcloud)
