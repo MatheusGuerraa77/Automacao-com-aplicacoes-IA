1a76bc182d6a062e81bc836095b51a0cfaa51e93
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-22 01:08:49 -0300

    Update 5-analise_imagem.py

--- 5-analise_imagem.py
+++ 5-analise_imagem.py
@@ -7,3 +7,20 @@ img = Image.open('data/homem-aranha.jpg')
 
 gray_img = img.convert('L')
 gray_img.show()
+
+# 2 - Utilizando Operações em Imagem
+rotated_img = img.rotate(180)
+rotated_img.show()
+
+# transpose_img = img.transpote(Image.FLIP_LEFT_RIGHT)
+# transpose_img.show()
+
+resize_img_small = img.resize((300,200))
+resize_img_small.show()
+
+resize_img_big = img.resize((1500, 1000))
+resize_img_big.show()
+
+dim = (200, 200, 250, 300)
+crop_img = img.crop(dim)
+crop_img.show()
